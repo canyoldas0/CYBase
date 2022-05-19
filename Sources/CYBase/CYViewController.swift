48eb9d1ecf0b0fac2c4c0496054714fbe828b5f0
Can Yoldas <can.yoldas@adesso.com.tr>
2022-05-20 00:51:29 +0300

    indicator added.

--- Sources/CYBase/CYViewController.swift
+++ Sources/CYBase/CYViewController.swift
@@ -14,6 +14,13 @@ open class CYViewController<V>: UIViewController {
     
     public var viewModel: V!
     
+    lazy var loadingView: UIActivityIndicatorView = {
+        let temp = UIActivityIndicatorView(style: .medium)
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.isHidden = true
+        return temp
+    }()
+    
     public convenience init(viewModel: V) {
         self.init()
         self.viewModel = viewModel
@@ -21,8 +28,31 @@ open class CYViewController<V>: UIViewController {
     
     open override func viewDidLoad() {
         super.viewDidLoad()
+        setupIndicatorView()
         setupVC()
         configureUI()
+    }
+    
+    
+    private func setupIndicatorView() {
+        view.addSubview(loadingView)
+        
+        NSLayoutConstraint.activate([
+            
+            loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loadingView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+    }
+    
+    open func startLoading() {
+        loadingView.startAnimating()
+        loadingView.isHidden = false
+    }
+    
+    open func stopLoading() {
+        loadingView.stopAnimating()
+        loadingView.isHidden = true
     }
     
     open func setupVC() { }
